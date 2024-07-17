f6f604ee6bf75fade7b2f626ce209f9a669ac5ef
Haroun Bouhroude <haroun19871987@gmail.com>
2024-07-17 20:23:33 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -345,22 +345,7 @@ function Home() {
           </div>
         </motion.div>
         {/* Testimonials */}
-        <motion.div
-          className="min-h-fit w-full"
-          initial={{ opacity: 0, x: 50 }}
-          animate={{ opacity: [0, 0.7, 1], x: 0 }}
-          transition={{
-            duration: 0.6,
-            ease: "easeInOut",
-            delay: 0.25,
-            type: "spring",
-          }}
-        >
-          <h1 className="text-2xl text-white font-bold uppercase pl-2 py-3 mb-2">
-            Testimonials
-          </h1>
-          <Slider />
-        </motion.div>
+        
       </div>
     </section>
   );
