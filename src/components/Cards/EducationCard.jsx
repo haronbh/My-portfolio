998e92225cd3a3081287968c57ce30e0c72285c5
Haroun Bouhroude <haroun19871987@gmail.com>
2024-07-17 01:45:38 +0100

    Update EducationCard.jsx

--- src/components/Cards/EducationCard.jsx
+++ src/components/Cards/EducationCard.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function EducationCard({
   title,
   year,
@@ -6,7 +8,6 @@ function EducationCard({
   description_1,
   description_2,
   description_3,
-
 }) {
   return (
     <div className="w-full min-h-fit p-4 bg-dark-200 rounded-3xl">
@@ -19,16 +20,22 @@ function EducationCard({
           <p className="text-md text-gray-300 font-light">{organization}</p>
         </div>
         <ul className="w-full list-disc px-4 flex flex-col gap-2 font-light">
-          <li className="text-md text-gray-200">{description_1}</li>
-          <li className="text-md text-gray-200">{description_2}</li>
-          <li className="text-md text-gray-200">{description_3}</li>          
+          {description_1 && (
+            <li className="text-md text-gray-200">{description_1}</li>
+          )}
+          {description_2 && (
+            <li className="text-md text-gray-200">{description_2}</li>
+          )}
+          {description_3 && (
+            <li className="text-md text-gray-200">{description_3}</li>
+          )}
         </ul>
         <div className="px-4">
           <p className="text-sm">{year}</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default EducationCard
+export default EducationCard;
